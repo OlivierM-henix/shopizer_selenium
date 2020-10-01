c37254d5d6fd855431ad245e3714ec8e120ec9b2
Olivier M <omonjauze@henix.fr>
2020-10-01 10:43:13 +0200

    Modification IConstantes.java : URLSite localhost

--- src/main/java/src/main/java/outilsTechniques/IConstantes.java
+++ src/main/java/src/main/java/outilsTechniques/IConstantes.java
@@ -6,8 +6,9 @@ public interface IConstantes {
 	String repertoire_racine_projet = "";
 	
 //	URL SITE :
-	String URLSite = "http://85.171.20.78:66/shop";
-//	String URLSite = "http://localhost:8080/shop";
+
+//	String URLSite = "http://85.171.20.78:66/shop";
+	String URLSite = "http://localhost:8080/shop";
 
 //	IDENTIFIANTS ET JDD
 
